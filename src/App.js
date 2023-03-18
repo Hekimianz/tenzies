b74c0c05597b9de3daf24c5a706fea15a7cc6be0
Aram Hekimian <aramhek121@gmail.com>
2023-03-17 18:48:54 -0600

    basic roll implemented

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
 import React from "react";
+import Die from "./components/Die";
+import { nanoid } from "nanoid";
 
 export default function App() {
-  return <main></main>;
+  const [dice, setDice] = React.useState(allNewDice);
+
+  function allNewDice() {
+    let arr = [];
+    for (let i = 0; i < 10; i++) {
+      arr.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid(),
+      });
+    }
+    return arr;
+  }
+
+  const diceElements = dice.map((item) => {
+    return <Die num={item.value} key={item.id} />;
+  });
+
+  function roll() {
+    setDice(allNewDice);
+  }
+
+  return (
+    <main>
+      <div className="die--cont">{diceElements}</div>
+      <button className="roll--btn" onClick={roll}>
+        Roll
+      </button>
+    </main>
+  );
 }
